40fcadf1319a1ff6e202fe8a94de640e67b2af02
David Freidin <36039896+dfreidin@users.noreply.github.com>
2021-05-06 21:26:45 -0700

    fix subdomain for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "game_night_#{Rails.env}"
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = {host: 'freidin.net'}
+  config.action_mailer.default_url_options = {host: 'gameknight.freidin.net'}
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     address:               ENV["SES_SERVER"],
